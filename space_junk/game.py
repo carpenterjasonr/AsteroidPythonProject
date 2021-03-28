00bf646d88712018ab78e6ac52a0ca02c0dcdf9e
Jason Carpenter <48468892+carpenterjasonr@users.noreply.github.com>
2021-03-27 22:56:04 -0700

    added random start positions for asteroids, collision handling, spaceship delete on collision

--- space_junk/game.py
+++ space_junk/game.py
@@ -1,15 +1,27 @@
 import pygame
 
-from models import GameObject
-from utils import load_sprite
+from models import Spaceship, Asteroid
+from utils import get_random_position, load_sprite
 
 class SpaceJunk:
+    MIN_ASTEROID_DISTANCE = 250
+
     def __init__(self):
         self._init_pygame()
         self.screen = pygame.display.set_mode((800, 600))
         self.background = load_sprite("space", False)
-        self.spaceship = GameObject((400,300), load_sprite("spaceship"), (0,0))
-        self.asteroid = GameObject((400,300), load_sprite("asteroid"), (1,0))
+        self.clock = pygame.time.Clock()
+
+        self.spaceship = Spaceship((400,300))
+        self.asteroids = []
+
+        for _ in range(6):
+            while True:
+                position = get_random_position(self.screen)
+                if (position.distance_to(self.spaceship.position) > self.MIN_ASTEROID_DISTANCE):
+                    break
+
+            self.asteroids.append(Asteroid(position))
 
     def main_loop(self):
         while True:
@@ -25,13 +37,38 @@ class SpaceJunk:
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 quit()
+            
+            is_key_pressed = pygame.key.get_pressed()
+
+            if self.spaceship:
+                if is_key_pressed[pygame.K_RIGHT]:
+                    self.spaceship.rotate(clockwise=True)
+                elif is_key_pressed[pygame.K_LEFT]:
+                    self.spaceship.rotate(clockwise=False)
+                if is_key_pressed[pygame.K_UP]:
+                    self.spaceship.accelerate()
 
     def _process_game_logic(self):
-        self.spaceship.move()
-        self.asteroid.move()
+        for game_object in self._get_game_objects():
+            game_object.move(self.screen)
+
+        if self.spaceship:
+            for asteroid in self.asteroids:
+                if asteroid.collides_with(self.spaceship):
+                    self.spaceship = None
+                    break
 
     def _draw(self):
         self.screen.blit(self.background, (0, 0))
-        self.spaceship.draw(self.screen)
-        self.asteroid.draw(self.screen)
+        for game_object in self._get_game_objects():
+            game_object.draw(self.screen)
         pygame.display.flip()
+        self.clock.tick(60)
+
+    def _get_game_objects(self):
+        game_objects = [*self.asteroids]
+
+        if self.spaceship:
+            game_objects.append(self.spaceship)
+        
+        return game_objects
