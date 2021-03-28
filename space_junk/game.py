aba460ac63a77106de818bfcc7f95632abbe67fd
Jason Carpenter <48468892+carpenterjasonr@users.noreply.github.com>
2021-03-27 17:33:06 -0700

    added models.py to define init draw move and collision states for sprites

--- space_junk/game.py
+++ space_junk/game.py
@@ -1,4 +1,6 @@
 import pygame
+
+from models import GameObject
 from utils import load_sprite
 
 class SpaceJunk:
@@ -6,6 +8,8 @@ class SpaceJunk:
         self._init_pygame()
         self.screen = pygame.display.set_mode((800, 600))
         self.background = load_sprite("space", False)
+        self.spaceship = GameObject((400,300), load_sprite("spaceship"), (0,0))
+        self.asteroid = GameObject((400,300), load_sprite("asteroid"), (1,0))
 
     def main_loop(self):
         while True:
@@ -19,14 +23,15 @@ class SpaceJunk:
 
     def _handle_input(self):
         for event in pygame.event.get():
-            if event.type == pygame.QUIT or (
-                event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
-            ):
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 quit()
 
     def _process_game_logic(self):
-        pass
+        self.spaceship.move()
+        self.asteroid.move()
 
     def _draw(self):
         self.screen.blit(self.background, (0, 0))
+        self.spaceship.draw(self.screen)
+        self.asteroid.draw(self.screen)
         pygame.display.flip()
